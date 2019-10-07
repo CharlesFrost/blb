e4fbe6350f948dbceb83f3d7d00116d0566b32a9
Karol <mroziaczek00@gmail.com>
2019-10-08 00:44:58 +0200

    ranking enhanced

--- src/main/java/com/charlesfrost/blb/services/StatisticService.java
+++ src/main/java/com/charlesfrost/blb/services/StatisticService.java
@@ -8,8 +8,6 @@ import com.charlesfrost.blb.repositories.TeamRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
